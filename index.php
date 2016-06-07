8795b89b30cc1c633317320455bb00b716343aca
Reginaldo Junior <76regi@gmail.com>
2016-06-07 20:43:58 -0300

    cards and new backgound

--- index.php
+++ index.php
@@ -6,70 +6,230 @@
 
 	<!-- Compiled and minified CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="/assets/style.css">
 </head>
 <body>
 
-	<div class="header">
-		<div class="background">		
-			<div class="container">
-				<nav>
-					<div class="nav-wrapper">						
-						<ul id="nav-mobile" class="right hide-on-med-and-down">
-							<li>
-								<a href="sass.html">Cadastrar Protesto</a>
-							</li>
-							<li>
-								<a href="badges.html">Colabore</a>
-							</li>
+	<div class="container-flex">
+		<div class="row">
+			<div class="col s12 background">		
+				<div class="container">
+					<nav>
+						<div class="nav-wrapper">						
+							<ul id="nav-mobile" class="right hide-on-med-and-down">
 								<li>
-								<a href="collapsible.html">Contato</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
+									<a href="sass.html">Cadastrar Protesto</a>
+								</li>
+								<li>
+									<a href="badges.html">Colabore</a>
+								</li>
+									<li>
+									<a href="collapsible.html">Contato</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
 
-				<!-- Page Content goes here -->
-				<h1>Protestos Brasil</h1>
+					<!-- Page Content goes here -->
+					<h1>Democracia nas Ruas</h1>
 
-				<div class="row">
-					<div class="input-field col s12">
-						<input id="search" type="text" class="validate">
-						<label for="search">Procurar Protesto</label>
+					<div class="row">
+						<div class="input-field col s12">
+							<input id="search" type="text" class="validate">
+							<label for="search">Procurar Protesto</label>
+						</div>
 					</div>
+					<div class="row">
+						
+						<div class="input-field col s6">
+							<select>
+								<option value="">Escolha o estado</option>
+								<option value="1">Option 1</option>
+								<option value="2">Option 2</option>
+								<option value="3">Option 3</option>
+							</select>
+							<label>Filtro de Estado</label>
+						</div>
+
+						<div class="input-field col s6">
+							<select multiple>
+								<option value="">Escolha a cidade</option>
+								<option value="1">Option 1</option>
+								<option value="2">Option 2</option>
+								<option value="3">Option 3</option>
+							</select>
+							<label>Filtro de Cidade</label>
+						</div>
+
+					</div>
+
+					<div class="row">
+					 	<div class="col s12 text-center">
+							<a class="waves-effect waves-light btn-large red">Procurar</a>
+						</div>
+					</div>
+
 				</div>
-				<div class="row">
-					
-					<div class="input-field col s6">
-						<select>
-							<option value="">Escolha o estado</option>
-							<option value="1">Option 1</option>
-							<option value="2">Option 2</option>
-							<option value="3">Option 3</option>
-						</select>
-						<label>Filtro de Estado</label>
-					</div>
-
-					<div class="input-field col s6">
-						<select multiple>
-							<option value="">Escolha a cidade</option>
-							<option value="1">Option 1</option>
-							<option value="2">Option 2</option>
-							<option value="3">Option 3</option>
-						</select>
-						<label>Filtro de Cidade</label>
-					</div>
-
-				</div>
-
-				<div class="row">
-				 	<div class="col s12 text-center">
-						<a class="waves-effect waves-light btn-large red">Procurar</a>
-					</div>
-				</div>
-
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m3">
+				<div class="card blue-grey darken-1">
+		            <div class="card-image">
+		            	<img src="https://d3oilk4eevknv6.cloudfront.net/newsweb/mediatools/image/image/000/001/814/464_1280x853_282157931_900x600.jpg">
+		            </div>
+					<div class="card-content white-text">
+						<span class="card-title">Defesa a escola publica</span>
+						<p>Por Mídia NINJA com colaboração do estudante Nathan Follmann, um dos líderes do movimento de ocupação na cidade.</p>
+					</div>
+					<div class="card-action text-right">
+						<a class="waves-effect waves-light btn-floating blue darken-1"><i class="fa fa-eye"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-2 cicle"><i class="fa fa-twitter"></i></a>
+						<a class="waves-effect waves-light btn-floating blue darken-3 cicle"><i class="fa fa-facebook"></i></a>
+						<a class="waves-effect waves-light btn-floating red cicle"><i class="fa fa-google-plus-official"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+            
 	</div>
 	        
 
